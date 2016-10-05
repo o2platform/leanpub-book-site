8dfbe39fef7e8333c61a03d6b515d80fbbcd27e5
Dinis Cruz <dinis.cruz@owasp.org>
2016-10-05 01:32:02 +0100

    Adding build files

--- build-book.sh
+++ build-book.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+echo ***** Pulling latest Changes *****
 cd book/Book_Jira_Risk_Workflow
 git pull origin master
 cd ../..
 
+echo ***** Building Changes *****
 ./bin/Create-Book.coffee
 
+echo ***** Publishing Book Changes *****
 cd book/Book_Jira_Risk_Workflow_Build
 git add -A
 git commit -m "Adding build files"
